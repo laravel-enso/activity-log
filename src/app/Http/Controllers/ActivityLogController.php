d2a7a2cfd4f785dfa72faea3389d09bb5c574ae8
mishu <mishu@xtelecom.ro>
2018-09-24 18:09:14 +0300

    fixes controller import

--- src/app/Http/Controllers/ActivityLogController.php
+++ src/app/Http/Controllers/ActivityLogController.php
@@ -2,7 +2,7 @@
 
 namespace LaravelEnso\ActivityLog\app\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use Illuminate\Routing\Controller;
 use LaravelEnso\ActivityLog\app\Http\Responses\Feed;
 
 class ActivityLogController extends Controller
