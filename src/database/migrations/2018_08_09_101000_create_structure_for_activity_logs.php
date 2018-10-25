d48c69f74ca92d87898793286bb5381b75d31250
mishu <mishu@xtelecom.ro>
2018-10-25 08:13:54 +0300

    drops the `permissionGroup` property. updates composer.json

--- src/database/migrations/2018_08_09_101000_create_structure_for_activity_logs.php
+++ src/database/migrations/2018_08_09_101000_create_structure_for_activity_logs.php
@@ -4,14 +4,12 @@ use LaravelEnso\StructureManager\app\Classes\StructureMigration;
 
 class CreateStructureForActivityLogs extends StructureMigration
 {
-    protected $permissionGroup = ['name' => 'core.activityLogs', 'description' => 'ActivityLogs Permission Group'];
-
     protected $permissions = [
         ['name' => 'core.activityLogs.index', 'description' => 'Show index for activity log', 'type' => 0, 'is_default' => false],
     ];
 
     protected $menu = [
-        'name' => 'Activity Log', 'icon' => 'shoe-prints', 'link' => 'core.activityLogs.index', 'order_index' => 900, 'has_children' => false,
+        'name' => 'Activity Log', 'icon' => 'shoe-prints', 'route' => 'core.activityLogs.index', 'order_index' => 900, 'has_children' => false,
     ];
 
     protected $parentMenu = '';
