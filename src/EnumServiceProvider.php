607ba7f1cdf2f583aceaecb6bdda31acab076543
mishu <mishu@xtelecom.ro>
2019-10-03 19:20:57 +0300

    resolves loggableEvents from service container

--- src/EnumServiceProvider.php
+++ src/EnumServiceProvider.php
@@ -2,12 +2,16 @@
 
 namespace LaravelEnso\ActivityLog;
 
+use Illuminate\Support\Facades\App;
 use LaravelEnso\ActivityLog\app\Enums\Events;
 use LaravelEnso\Enums\EnumServiceProvider as ServiceProvider;
 
 class EnumServiceProvider extends ServiceProvider
 {
-    public $register = [
-        'loggableEvents' => Events::class,
-    ];
+    public function boot()
+    {
+        $this->register[] = App::make(Events::class)->all();
+
+        parent::boot();
+    }
 }
