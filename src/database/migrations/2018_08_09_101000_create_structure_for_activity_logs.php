585a6964f27cf1f1f11883e1f8d9078fe289ed10
mishu <mishu@xtelecom.ro>
2020-03-14 18:03:31 +0200

    removes Types from migration

--- src/database/migrations/2018_08_09_101000_create_structure_for_activity_logs.php
+++ src/database/migrations/2018_08_09_101000_create_structure_for_activity_logs.php
@@ -1,12 +1,11 @@
 <?php
 
 use LaravelEnso\Migrator\App\Database\Migration;
-use LaravelEnso\Permissions\App\Enums\Types;
 
 class CreateStructureForActivityLogs extends Migration
 {
     protected $permissions = [
-        ['name' => 'core.activityLogs.index', 'description' => 'Show index for activity log', 'type' => Types::Read, 'is_default' => false],
+        ['name' => 'core.activityLogs.index', 'description' => 'Show index for activity log', 'is_default' => false],
     ];
 
     protected $menu = [
